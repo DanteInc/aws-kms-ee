1c68e4ffa1c5c405f447f73403de99401e75190b
jgilbert01 <jgilbert01@yahoo.com>
2019-08-06 14:37:26 -0400

    added memoizee

--- src/connector.js
+++ src/connector.js
@@ -1,4 +1,5 @@
 import { KMS } from 'aws-sdk';
+import memoizee from 'memoizee';
 
 import { debug } from './utils';
 
@@ -13,24 +14,49 @@ class Connector {
   }
 
   generateDataKey() {
-    return this.kms.generateDataKey({
-      KeyId: this.masterKeyAlias,
-      KeySpec: 'AES_256',
-    }).promise();
+    return this._generateDataKey();
   }
 
   decryptDataKey(dataKey) {
-    return this.kms.decrypt({
-      CiphertextBlob: Buffer.from(dataKey, 'base64'),
-    }).promise();
+    return this._decryptDataKey(dataKey);
   }
 
   encryptDataKey(plainText) {
-    return this.kms.encrypt({
-      KeyId: this.masterKeyAlias,
-      Plaintext: plainText,
-    }).promise();
+    return this._encryptDataKey(plainText);
   }
+
+  _generateDataKey = memoizee(
+    () =>
+      this.kms.generateDataKey({
+        KeyId: this.masterKeyAlias,
+        KeySpec: 'AES_256',
+      }).promise(),
+    {
+      promise: true,
+      maxAge: Number(process.env.DATA_KEY_MAX_AGE) || 1800000, // 30 minutes
+    },
+  );
+
+  _decryptDataKey = memoizee(
+    dataKey =>
+      this.kms.decrypt({
+        CiphertextBlob: Buffer.from(dataKey, 'base64'),
+      }).promise(),
+    {
+      promise: true,
+    },
+  );
+
+  _encryptDataKey = memoizee(
+    plainText =>
+      this.kms.encrypt({
+        KeyId: this.masterKeyAlias,
+        Plaintext: plainText,
+      }).promise(),
+    {
+      promise: true,
+    },
+  );
 }
 
 export default Connector;
